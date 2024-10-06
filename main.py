2f11d66e5f913ad2f59f13ee9018473bcad3a1f7
KlopfKlopf9 <linus93@live.de>
2024-10-06 09:34:27 +0200

    A: Setup main.py and constants for Asteroid project

---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting asteroids!")
+    print("Starting asteroids!\nScreen width: 1280\nScreen height: 720")
+
+if __name__=="__main__":
+    main()
