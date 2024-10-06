adf9b2378e37fa83a0e9d5ea0755906f2aee3b69
KlopfKlopf9 <linus93@live.de>
2024-10-06 10:11:24 +0200

    Create basi  Gamewindow

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill((0,0,0,1))
+        pygame.display.flip()
     print("Starting asteroids!")
     print("Starting asteroids!\nScreen width: 1280\nScreen height: 720")
 
