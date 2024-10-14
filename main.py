f8a39967b91374b0609381fdfbd3400046f9ddf2
KlopfKlopf9 <linus93@live.de>
2024-10-14 17:57:40 +0200

    Add ability to move the Player and the Asteroids to spawn and render on screen

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable)
+
+
     player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+    asteroidfield = AsteroidField()
+    dt = 0
+    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
+        for sprite in updateable:
+            sprite.update(dt)
+        
         screen.fill((0,0,0,1))
-        player.draw(screen)
+        
+        for sprite in drawable:
+            sprite.draw(screen)
+            
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
